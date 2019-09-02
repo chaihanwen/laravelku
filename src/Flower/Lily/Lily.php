be6f427ceab9f7848de67e5fc71f063472cb7174
chaihanwen <xue19920819@163.com>
2019-09-02 16:55:54 +0800

    安装自己的包！

---
+++ src/Flower/Lily/Lily.php
@@ -0,0 +1,16 @@
+<?php
+
+	namespace Flower\Lily;
+	
+	
+	Class Lily{
+		
+		public function xingming(){
+			echo "my name is xuehanwen!"
+		}
+		
+	}
+
+
+
+?>
---
+++ src/Flower/Rose/Rose.php
@@ -0,0 +1,16 @@
+<?php
+
+	namespace Flower\Rose;
+	
+	
+	Class Rose{
+		
+		public function xingming(){
+			echo "my name is chaiaixue!"
+		}
+		
+	}
+
+
+
+?>
